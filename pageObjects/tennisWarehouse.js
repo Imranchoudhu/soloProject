8ba68e5e55ea24892fbce796ef95cc69172375ec
Imranchoudhu <imranchoudhu@gmail.com>
2019-07-16 12:57:09 -0600

    updateTennis

--- pageObjects/tennisWarehouse.js
+++ pageObjects/tennisWarehouse.js
@@ -1,10 +1,61 @@
+var lacoste = {
+    apparelFilter: function (add) {
+        this
+            .waitForElementPresent('@searchBtn', 5000)
+            .setValue('@searchBtn', add.name)
+            .click('@searchIcon')
+            .waitForElementPresent('@result', 5000)
+            .verify.containsText('@result', add.name)
+            .click('@filter')
+            .click('@lowestToHigest')
+            .waitForElementPresent('@result', 5000)
+            .verify.containsText('@result', add.filtered)
+        return this
+    },
+    adidasFilter: function (add) {
+        this
+            .waitForElementPresent('@searchBtn', 5000)
+            .clearValue('@searchBtn')
+            .setValue('@searchBtn', add.name)
+            .click('@searchIcon')
+            .waitForElementPresent('@result', 5000)
+            .verify.containsText('@result', add.name)
+            .click('@filter')
+            .click('@highestToLowest')
+            .waitForElementPresent('@result', 5000)
+            .verify.containsText('@result', add.filtered)
+        return this
+    },
+    nikeFilter: function (add){
+        this
+            .waitForElementPresent('@searchBtn', 5000)
+            .clearValue('@searchBtn')
+            .setValue('@searchBtn', add.name)
+            .click('@searchIcon')
+            .waitForElementPresent('@result', 5000)
+            .verify.containsText('@result', add.name)
+            .click('@filter')
+            .click('@sortaToz')
+            .waitForElementPresent('@result', 5000)
+            .verify.containsText('@result', add.filtered)
+        return this
+
+
+
+    }
+
+
+
+
+}
 module.exports = {
     url: 'https://www.tennis-warehouse.com/',
-    elements:{
-        searchBtn:'#searchtext',
-        logIn:'.ac_login',
-        result:'.fl',
-        searchIcon:'#search_icon',
+    commands: [lacoste],
+    elements: {
+        searchBtn: '#searchtext',
+        logIn: '.ac_login',
+        result: '.fl',
+        searchIcon: '#search_icon',
         filter: {
             selector: '(//*[@class="ui-icon ui-icon-triangle-white-s"])[1]',
             locateStrategy: 'xpath'
@@ -14,7 +65,17 @@ module.exports = {
             selector: '(//*[@class="ui-menu-item"])[2]',
             locateStrategy: 'xpath'
         },
-        
+        highestToLowest: {
+            selector: '(//*[@class="ui-menu-item"])[3]',
+            locateStrategy: 'xpath'
+        },
+        sortaToz: {
+            selector: '(//*[@class="ui-menu-item"])[1]',
+            locateStrategy: 'xpath'
 
+
+
+
+    }
 }
 }
---
+++ tests/clothingTest.js
@@ -0,0 +1,22 @@
+var tennisPage = {}
+module.exports = {
+    beforeEach: browser =>{
+        tennisPage = browser.page.tennisWarehouse()
+        tennisPage.navigate()
+    },
+    after: browser => {
+        browser.end()
+    },
+
+     'Search and Filter': browser => {
+         tennisPage
+            .apparelFilter({name: 'Lacoste', filtered: 'Laco&#115;te' })
+            .adidasFilter({name: 'adidas', filtered: 'adida&#115'})
+            .nikeFilter({name: 'nike', filtered: 'nike'})
+
+            
+        },
+    
+    
+
+}
